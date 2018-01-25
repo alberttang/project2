dc84c59ac16cb7879b31592e2449f5e4642b9417
babershere <rehmatq@gmail.com>
2018-01-24 22:08:54 -0800

    response get request

--- routes/model-routes/response-route.js
+++ routes/model-routes/response-route.js
@@ -44,7 +44,7 @@ module.exports = function (app) {
                 id: req.params.id
             }
         }).then(function (dbAuthor) {
-            res.json(dbAuthor);
+            // res.json(dbAuthor);
         });
     }); // END GET 
 
@@ -52,19 +52,19 @@ module.exports = function (app) {
         ================= DELETE ==================== 
     */
 
-    // DELETE route for deleting responses
-    app.delete("/api/response/:id", function (req, res) {
-        // CONSOLE LOG RTHE REQUEST OBJ
-        console.log(req.body);
-        // DELETE THE ID IN THE DB
-        db.Response.destroy({
-                where: {
-                    id: req.params.id
-                }
-            })
-            .then(function (dbPost) {
-                res.json(dbPost);
-            });
-    }); // END DELETE
+    // // DELETE route for deleting responses
+    // app.delete("/api/response/:id", function (req, res) {
+    //     // CONSOLE LOG RTHE REQUEST OBJ
+    //     console.log(req.body);
+    //     // DELETE THE ID IN THE DB
+    //     db.Response.destroy({
+    //             where: {
+    //                 id: req.params.id
+    //             }
+    //         })
+    //         .then(function (dbPost) {
+    //             res.json(dbPost);
+    //         });
+    // }); // END DELETE
 
 }; // END EXPORT
