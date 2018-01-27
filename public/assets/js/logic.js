35721a1529af0b1b4d844336d6b9099c5abb384c
Albert Tangtheinkul <bamf_albert@yahoo.com>
2018-01-27 00:14:57 -0800

    added logic to log user responses

--- public/assets/js/logic.js
+++ public/assets/js/logic.js
@@ -7,117 +7,129 @@ $('.vote-button').click(function () {
     $('.vote-counter').html(votes);
     console.log(votes);
 })
+/////////////////////////////////////////////////////////////////////////////////////////////
+//FUNCTION TO POPULATE POLLS ON THE INDEX PAGE//////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+// function populatePoll(question, category, option1, pID, oID) {
+//     // $( ".test-span" ).append( "<p>Test</p>" );
+//     // var per = "personal"
+//     var per = category;
+//     var personlOptions = $("#" + per);    
+//     var personalInput = $("<input type='radio' name='option'> ");
+//     var perSpan = $("<span>");
+//     personalInput.attr("value", option1);
+//     var pollId = pID;
+//     var optid = oID;
+//     personalInput.attr("pollId", pID);
+//     personalInput.attr("optid", oID);
+//     perSpan.html(option1);
+//     personlOptions.append(personalInput);
+//     personalInput.after(perSpan);
+//     var ques1 = $("." + per + "-question");
+//     ques1.html(question);
 
-// trying to figure out how to remove login form on page load
-
-function populatePoll(question, category, option1) {
-    // $( ".test-span" ).append( "<p>Test</p>" );
-    // var per = "personal"
-    var per = category;
-    var personlOptions = $("#" + per);
-    var personalInput = $("<input type='radio' name='option'> ");
-    var perSpan = $("<span>");
-    personalInput.attr("value", option1);
-    perSpan.html(option1);
-    personlOptions.append(personalInput);
-    personalInput.after(perSpan);
-    var ques1 = $("." + per + "-question");
-    ques1.html(question);
-
-    console.log(option1);
-    console.log(category);
-    console.log(question);
+//     console.log(option1);
+//     console.log(category);
+//     console.log(question);
 
 
-};
+// };
+
+/////////////////////////////////////////////////////////////////////////////////////////
+// Function to Get Polls for each Category /////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////
+
+// $(document).ready(function () {
+
+//     axios.get('/api/poll-category-search/personal')
+//         .then(function (response) {
+//             console.log(response.data);
+//             var parQues = response.data[0].question;
+//             // var parOpt1 = response.data[0].Answers[0].option;
+//             // var parOpt2 = response.data[0].Answers[1].option;
+//             // var parOpt3 = response.data[0].Answers[2].option;
+//             // //   var parOpt4 =response.data[0].Answers[3].option;
+//             console.log(response.data[0].Answers.length);
+//             for (i = 0; i < response.data[0].Answers.length; i++) {
+//                 populatePoll(parQues, "personal", response.data[0].Answers[i].option, response.data[0].Answers[i].PollId, response.data[0].Answers[i].id);
+//             }
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+
+//     axios.get('/api/poll-category-search/entertainment')
+//         .then(function (response) {
+//             console.log(response.data);
+//             var parQues = response.data[0].question;
+//             // var parOpt1 = response.data[0].Answers[0].option;
+//             // var parOpt2 = response.data[0].Answers[1].option;
+//             // var parOpt3 = response.data[0].Answers[2].option;
+//             // //   var parOpt4 =response.data[0].Answers[3].option;
+//             console.log(response.data[0].Answers.length);
+//             for (i = 0; i < response.data[0].Answers.length; i++) {
+//                 populatePoll(parQues, "entertainment", response.data[0].Answers[i].option, response.data[0].Answers[i].PollId, response.data[0].Answers[i].id);
+//             }
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+//     axios.get('/api/poll-category-search/science')
+//         .then(function (response) {
+//             console.log(response.data);
+//             var parQues = response.data[0].question;
+//             // var parOpt1 = response.data[0].Answers[0].option;
+//             // var parOpt2 = response.data[0].Answers[1].option;
+//             // var parOpt3 = response.data[0].Answers[2].option;
+//             // //   var parOpt4 =response.data[0].Answers[3].option;
+//             console.log(response.data[0].Answers.length);
+//             for (i = 0; i < response.data[0].Answers.length; i++) {
+//                 populatePoll(parQues, "science", response.data[0].Answers[i].option, response.data[0].Answers[i].PollId, response.data[0].Answers[i].id);
+//             }
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+//     axios.get('/api/poll-category-search/philosophy')
+//         .then(function (response) {
+//             console.log(response.data);
+//             var parQues = response.data[0].question;
+//             // var parOpt1 = response.data[0].Answers[0].option;
+//             // var parOpt2 = response.data[0].Answers[1].option;
+//             // var parOpt3 = response.data[0].Answers[2].option;
+//             // //   var parOpt4 =response.data[0].Answers[3].option;
+//             console.log(response.data[0].Answers.length);
+//             for (i = 0; i < response.data[0].Answers.length; i++) {
+//                 populatePoll(parQues, "philosophy", response.data[0].Answers[i].option, response.data[0].Answers[i].PollId, response.data[0].Answers[i].id);
+//             }
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+//     axios.get('/api/poll-category-search/world')
+//         .then(function (response) {
+//             console.log(response.data);
+//             var parQues = response.data[0].question;
+//             // var parOpt1 = response.data[0].Answers[0].option;
+//             // var parOpt2 = response.data[0].Answers[1].option;
+//             // var parOpt3 = response.data[0].Answers[2].option;
+//             // //   var parOpt4 =response.data[0].Answers[3].option;
+//             console.log(response.data[0].Answers.length);
+//             for (i = 0; i < response.data[0].Answers.length; i++) {
+//                 populatePoll(parQues, "world", response.data[0].Answers[i].option, response.data[0].Answers[i].PollId, response.data[0].Answers[i].id);
+//             }
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+
+// });
+////////////////////////////////////////////////////////////////////////
 
 
-$(document).ready(function () {
-
-    axios.get('/api/poll-category-search/personal')
-        .then(function (response) {
-            console.log(response.data);
-            var parQues = response.data[0].question;
-            // var parOpt1 = response.data[0].Answers[0].option;
-            // var parOpt2 = response.data[0].Answers[1].option;
-            // var parOpt3 = response.data[0].Answers[2].option;
-            // //   var parOpt4 =response.data[0].Answers[3].option;
-            console.log(response.data[0].Answers.length);
-            for ( i = 0; i < response.data[0].Answers.length; i++){
-                populatePoll(parQues, "personal", response.data[0].Answers[i].option);                
-            }           
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-
-        axios.get('/api/poll-category-search/entertainment')
-        .then(function (response) {
-            console.log(response.data);
-            var parQues = response.data[0].question;
-            // var parOpt1 = response.data[0].Answers[0].option;
-            // var parOpt2 = response.data[0].Answers[1].option;
-            // var parOpt3 = response.data[0].Answers[2].option;
-            // //   var parOpt4 =response.data[0].Answers[3].option;
-            console.log(response.data[0].Answers.length);
-            for ( i = 0; i < response.data[0].Answers.length; i++){
-                populatePoll(parQues, "entertainment", response.data[0].Answers[i].option);                
-            }           
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-        axios.get('/api/poll-category-search/science')
-        .then(function (response) {
-            console.log(response.data);
-            var parQues = response.data[0].question;
-            // var parOpt1 = response.data[0].Answers[0].option;
-            // var parOpt2 = response.data[0].Answers[1].option;
-            // var parOpt3 = response.data[0].Answers[2].option;
-            // //   var parOpt4 =response.data[0].Answers[3].option;
-            console.log(response.data[0].Answers.length);
-            for ( i = 0; i < response.data[0].Answers.length; i++){
-                populatePoll(parQues, "science", response.data[0].Answers[i].option);                
-            }           
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-        axios.get('/api/poll-category-search/philosophy')
-        .then(function (response) {
-            console.log(response.data);
-            var parQues = response.data[0].question;
-            // var parOpt1 = response.data[0].Answers[0].option;
-            // var parOpt2 = response.data[0].Answers[1].option;
-            // var parOpt3 = response.data[0].Answers[2].option;
-            // //   var parOpt4 =response.data[0].Answers[3].option;
-            console.log(response.data[0].Answers.length);
-            for ( i = 0; i < response.data[0].Answers.length; i++){
-                populatePoll(parQues, "philosophy", response.data[0].Answers[i].option);                
-            }           
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-        axios.get('/api/poll-category-search/world')
-        .then(function (response) {
-            console.log(response.data);
-            var parQues = response.data[0].question;
-            // var parOpt1 = response.data[0].Answers[0].option;
-            // var parOpt2 = response.data[0].Answers[1].option;
-            // var parOpt3 = response.data[0].Answers[2].option;
-            // //   var parOpt4 =response.data[0].Answers[3].option;
-            console.log(response.data[0].Answers.length);
-            for ( i = 0; i < response.data[0].Answers.length; i++){
-                populatePoll(parQues, "world", response.data[0].Answers[i].option);                
-            }           
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-
-});
-
+///////////////////////////////////////////////////////////////////////
+//FUNCTION FOR LOGIN//////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
 $(function () {
     $(".login").on("click", function (event) {
         event.preventDefault();
@@ -150,6 +162,11 @@ $(function () {
     })
 })
 
+
+
+//////////////////////////////////////////////////////////////////////////
+//FUNCTION TO CREATE A POLL AND POST TO SQL////////
+/////////////////////////////////////////////////////////////////////////
 $(function () {
     $(".create-poll").on("click", function (event) {
 
@@ -232,7 +249,47 @@ $(function () {
 });
 
 
+//////////////////////////////////////////////////////////////////////////////////////////
+//FUNCTION TO POST VOTE TO RESPONSES TABLE /////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+$(function () {
+    $(".vote-button").on("click", function (event) {
 
+        var radioValue = $("input[name='option']:checked").val();
+        if (!radioValue) {
+            alert("Please Select an Answer");
+        } else {
+            console.log(radioValue);
+            var pollId = $("input[name='option']:checked").attr("pollId");
+            var ansID = $("input[name='option']:checked").attr("optid");
+            var userID = $("input[name='option']:checked").attr("pollId");
+            var newResponse = {
+                "answerId": ansID,
+                "PollId": pollId,
+                "UserId": sessionStorage.userId
+            };
+            console.log(newResponse);
+
+            axios.post('/api/response', newResponse, { headers: { Authorization: sessionStorage.jwt } })
+                    .then(function (response) {
+                        // var pollID = response.data[0].PollId;
+                        // console.log("The Poll ID is : " + pollID);
+                        console.log(response);
+                        // window.location.replace('/api/poll/' + pollID);
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+
+            }
+
+        });
+    });
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// FUNCTION TO CHECK IF USER IS SIGNED IN BEFORE VIEWING MY POLLS
+/////////////////////////////////////////////////////////////////////////////////////////////////
 $(function () {
     $(".my-polls").on("click", function (event) {
 
@@ -246,3 +303,6 @@ $(function () {
 });
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////
+// FUNCTION FOR DISPLAYING THE RESULTS////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
